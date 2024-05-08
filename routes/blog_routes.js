e83de4f7d813b5442308f4877bb3b3f7322f1126
vgalante2 <vgalante0@gmail.com>
2024-05-08 18:17:41 -0400

    login/register  successful

--- routes/blog_routes.js
+++ routes/blog_routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router()
 const Post = require('../models/Post')
+const User = require('../models/User')
 
 
 function isAuth(req, res, next) {
@@ -12,20 +13,25 @@ function isAuth(req, res, next) {
 
 
 // Create a post
-router.post('/posts', async (req, res) => {
+router.post('/', isAuth, async (req, res) => {
     try {
+
+        const userId = req.session.user_id;
         if (!req.session.user_id) {
             // If there's no user_id in the session, respond with an error or redirect
             return res.status(403).send("User must be logged in to post.");
         }
 
+        console.log("logged in user", userId)
+        
+
         const newPost = await Post.create({
             title: req.body.title,
             text: req.body.text,
-            user_id: req.session.user_id  // Ensure this is being correctly populated
+            user_id: userId // Ensure this is being correctly populated
         });
-
-        res.redirect('/');
+        console.log('Request body:', req.body);
+        res.redirect('/home');
     } catch (error) {
         console.error("Error in POST /home:", error);
         if (!res.headersSent) {
--- routes/view_routes.js
+++ routes/view_routes.js
@@ -12,11 +12,20 @@ function isAuth(req, res, next) {
 }
 
 router.get('/', async (req, res) => {
-    const post = await Post.findAll()
-    res.render('home', {
-        post: post.map(p => p.get({plain: true}) )
-    })
+    let userObj = {
+        isLoggedIn: req.body ? true : false,
+        user: req.user
+    }
 
+    res.render('home', userObj)
+
+})
+
+router.get('/posts', async (req, res) => {
+    const posts = await Post.findAll()
+    res.render('posts', {
+        posts: posts.map(p => p.get({plain: true}) )
+    })
 })
 
 router.get('/dashboard', (req, res) => {
@@ -56,16 +65,18 @@ router.get('/logout', async (req, res) => {
 })
 
 router.get('/user', isAuth, async (req, res) => {
-let userObj ={
-    isLoggedIn: true,
-    user: req.user
-}
+    if (!req.user) {
+        // Handle case where user is not set
+        return res.status(404).send("User not found");
+    }
+    const user = await User.findByPk(req.user.id);
+    let userObj = {
+        isLoggedIn: true,
+        user: req.user
+    };
+    res.render('dashboard', userObj);
+});
 
-  const user = await User.findByPk(req.user.id)
-
-  console.log(userObj)
-  res.render('dashboard', userObj)
-})
 
 
 module.exports = router
